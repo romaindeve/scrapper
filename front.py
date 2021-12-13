62c2b4d1aaa003b08f2e0dabe9c85a391b5d2c2d
Romain BIGNOTTI <romain.bignotti@sia-partners.com>
2021-12-13 18:57:12 +0100

    Scrapper with newspaper3k

--- front.py
+++ front.py
@@ -1,5 +1,20 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from newspaper import Article
 
-st.title('Hello world !')
+default_text = "Paste the url of the website you want to scrap"
+
+
+st.title('Online scrapper')
+
+st.session_state['url'] = st.text_area(
+        "Entrez un extrait d'article de journal", value=default_text)
+
+if st.session_state['url'] != default_text and len(st.session_state['url']) > 0 :
+    article = Article(st.session_state['url'])
+    article.download()
+    article.parse()
+    st.write(f"## {article.title}")
+    st.write(f"_{article.author}_")
+    st.write(f"{article.text}")
